9f96ecdfd78034e42d6915fff2084f4856235b44
kushal <kushaldeveloper@gmail.com>
2015-10-23 18:55:24 -0400

    make a threaded program
    from page 165 of modern operating systems with Java

--- src/com/github/kusl/java/HelloWorld.java
+++ src/com/github/kusl/java/HelloWorld.java
@@ -2,9 +2,15 @@ package com.github.kusl.java;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println(returnZero());
-	}
-	private static int returnZero() {
-		return 0;
+		Sum sumObject = new Sum();
+		// set values ourselves for now
+		int upper = 5;
+		Thread thread = new Thread(new Summation(upper, sumObject));
+		thread.start();
+		try {
+			thread.join();
+			System.out.println(sumObject.getSum());
+		}
+		catch (InterruptedException ie) {System.out.println(ie.getMessage());}
 	}
 }
---
+++ src/com/github/kusl/java/Sum.java
@@ -0,0 +1,11 @@
+package com.github.kusl.java;
+
+public class Sum {
+    private int sum;
+    public int getSum(){
+        return sum;
+    }
+    public void setSum(int sum){
+        this.sum = sum;
+    }
+}
