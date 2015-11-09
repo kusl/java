f999216a9d9818eb6d8adee94152b17c3f9f0769
kushal <kushaldeveloper@gmail.com>
2015-11-08 19:13:19 -0500

    delete everything
    so we can experiment with something else

--- src/com/github/kusl/java/Assistant.java
+++
@@ -1,9 +0,0 @@
-package com.github.kusl.java;
-
-public class Assistant implements Runnable {
-
-    @Override
-    public void run() {
-
-    }
-}
--- src/com/github/kusl/java/Doctor.java
+++
@@ -1,9 +0,0 @@
-package com.github.kusl.java;
-
-public class Doctor implements Runnable {
-
-    @Override
-    public void run() {
-
-    }
-}
