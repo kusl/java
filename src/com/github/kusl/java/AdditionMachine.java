1726a14fa734950af2675e4f1db3e97df9093783
kushal <kushaldeveloper@gmail.com>
2015-11-08 19:25:55 -0500

    say hello world
    play with junit

---
+++ src/com/github/kusl/java/AdditionMachine.java
@@ -0,0 +1,11 @@
+package com.github.kusl.java;
+
+public class AdditionMachine {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public String add(String a, String b) {
+        return a + b;
+    }
+}
--- src/com/github/kusl/java/HelloWorld.java
+++ src/com/github/kusl/java/HelloWorld.java
@@ -2,5 +2,6 @@ package com.github.kusl.java;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("hello, world!");
     }
 }
