86b930072b5e43702ccf2c4bc42d2d5b2d110947
kushal <kushaldeveloper@gmail.com>
2015-11-24 09:59:28 -0500

    remove variable sleep

--- src/com/github/kusl/java/HelloWorld.java
+++ src/com/github/kusl/java/HelloWorld.java
@@ -3,10 +3,10 @@ package com.github.kusl.java;
 public class HelloWorld {
     public static void main(String[] args) {
         for (int i = 0; i < 9; i++) {
-            int sleep_time = MyUtility.get_a_number_between_min_and_max(10, 100);
-            System.out.println("We will now sleep for about " + sleep_time + " nano seconds.");
+//            int sleep_time = MyUtility.get_a_number_between_min_and_max(10, 100);
+            System.out.println(i);
             try {
-                java.lang.Thread.sleep(sleep_time);
+                java.lang.Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
